4686c2be2385ceb4b7cc0cf46ef56356ab4950a6
khai96_ <hvksmr1996@gmail.com>
2020-08-30 12:35:48 +0700

    Use status

--- index.test.js
+++ index.test.js
@@ -2,20 +2,21 @@ const { spawnSync } = require('child_process')
 const fs = require('fs')
 const isoGit = require('isomorphic-git')
 
-it('isoGit.listFiles() is the same as git-ls-files', async () => {
-  const { error, status, stdout } = spawnSync('git', ['ls-files'], {
+it('isoGit.statusMatrix() is the same as git-status', async () => {
+  const { error, status, stdout } = spawnSync('git', ['status', '--porcelain=v1'], {
     stdio: ['pipe', 'pipe', 'inherit'],
     encoding: 'utf8',
     cwd: __dirname,
   })
   if (error) throw error
   if (status) throw new Error(`Command 'git ls-files' exits with code ${status}`)
-  const fromNativeGit = new Set(stdout.split('\n').filter(Boolean))
+  const fromNativeGit = new Set(stdout.split('\n').filter(Boolean).map(line => line.slice(3)))
 
-  const fromIsoGit = new Set(await isoGit.listFiles({
-    fs,
-    dir: __dirname,
-  }))
+  const fromIsoGit = new Set(
+    (await isoGit.statusMatrix({ fs, dir: __dirname }))
+      .filter(([_, a, b, c]) => a !== 1 || b !== 1 || c !== 1) // filter changes
+      .map(([filename]) => filename)
+  )
 
   expect(fromIsoGit).toEqual(fromNativeGit)
 })
